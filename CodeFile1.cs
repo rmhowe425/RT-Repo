078a5a75edd9540a89e11494db8abfa669fec867
rmhowe425 <45905457+rmhowe425@users.noreply.github.com>
2020-12-08 08:09:26 -0500

    Add files via upload

---
+++ CodeFile1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+public class GetSocket
+{
+    private static Socket ConnectSocket(string server, int port)
+    {
+        Socket s = null;
+        IPHostEntry host = null;
+
+        foreach (IPAddress address in host.AddressList)
+        {
+            IPEndPoint ipe = new IPEndPoint(address, port);
+            Socket tempSocket =
+                new Socket(ipe.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            tempSocket.Connect(ipe);
+
+            if (tempSocket.Connected)
+            {
+                s = tempSocket;
+                break;
+            }
+            else
+            {
+                continue;
+            }
+        }
+        return s;
+    }
+    private static string SocketSendReceive(string server, int port)
+    {
+        string request = "GET / HTTP/1.1\r\nHost: " + server +
+            "\r\nConnection: Close\r\n\r\n";
+        Byte[] bytesSent = Encoding.ASCII.GetBytes(request);
+        Byte[] bytesReceived = new Byte[256];
+        string page = "";
+
+        // Create a socket connection with the specified server and port.
+        using (Socket s = ConnectSocket(server, port))
+        {
+
+            if (s == null)
+                return ("Connection failed");
+
+            // Send request to the server.
+            s.Send(bytesSent, bytesSent.Length, 0);
+
+            // Receive the server home page content.
+            int bytes = 0;
+            page = "Default HTML page on " + server + ":\r\n";
+
+            // The following will block until the page is transmitted.
+            do
+            {
+                bytes = s.Receive(bytesReceived, bytesReceived.Length, 0);
+                page = page + Encoding.ASCII.GetString(bytesReceived, 0, bytes);
+            }
+            while (bytes > 0);
+        }
+
+        return page;
+    }
+
+    public static void Main(string[] args)
+    {
+        string host;
+        int port = 80;
+
+
+        string result = SocketSendReceive(host, port);
+        Console.WriteLine(result);
+    }
+}
